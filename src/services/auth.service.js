d0b815e3a6afca9d6992c62eb0f5cfd2bd3620a6
Ayush Kumar <ltsaayushk193@gmail.com>
2025-10-19 19:52:56 +0530

    Implemented Sign in & Sign out

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -13,6 +13,15 @@ export const hashPassword = async (password) => {
     }
 };
 
+export const comparePassword = async (password, hashedPassword) =>  {
+    try{
+        return await bcrypt.compare(password, hashedPassword);
+    } catch (e) {
+        logger.error(`Error comparing password: ${e}`)
+        throw new Error('Error Comparing passwords')
+    }
+}
+
 export const createUser = async ({ name, email, password, role = 'user' }) => {
     try {
         const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
@@ -38,4 +47,39 @@ export const createUser = async ({ name, email, password, role = 'user' }) => {
         logger.error(`Error creating user: ${err.message}`);
         throw err;
     }
+};
+
+export const authenticateUser = async ({ email, password }) => {
+  try {
+    const [existingUser] = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (!existingUser) {
+      throw new Error('User not found');
+    }
+
+    const isPasswordValid = await comparePassword(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid password');
+    }
+
+    logger.info(`User ${existingUser.email} authenticated successfully`);
+    return {
+      id: existingUser.id,
+      name: existingUser.name,
+      email: existingUser.email,
+      role: existingUser.role,
+      created_at: existingUser.created_at,
+    };
+  } catch (e) {
+    logger.error(`Error authenticating user: ${e}`);
+    throw e;
+  }
 };
